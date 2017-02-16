8d172cf6df9f915473718b6928b483760ec25c36
Joshua Small <jksmall0631@gmail.com>
2017-02-16 15:08:54 -0700

    add most of the styling

--- app/components/reminder-card.js
+++ app/components/reminder-card.js
@@ -3,6 +3,9 @@ import Ember from 'ember';
 export default Ember.Component.extend({
   store: Ember.inject.service(),
 
+  tagName: 'article',
+  classNames: ['reminder-card-cont'],
+
   actions: {
     removeReminder(model) {
       model.destroyRecord();
--- app/components/reminders-list.js
+++ app/components/reminders-list.js
@@ -3,6 +3,9 @@ import Ember from 'ember';
 export default Ember.Component.extend({
   store: Ember.inject.service(),
 
+  tagName: 'article',
+  classNames: ['container'],
+
   actions: {
     removeReminder(reminder) {
       reminder.destroyRecord();
